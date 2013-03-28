e189942e58e0c08a99d677925fe91542f8ee2bbd
r61266 <lightking@fullsail.edu>
2013-03-28 06:59:50 -0400

    Added more files to repo.

---
+++ JS/json.js
@@ -0,0 +1,34 @@
+// JavaScript Document
+
+var json = {
+	"Birthday1": {
+		"fname": ["First Name:", "Alec"],
+		"lname": ["Last Name:", "Shallenberger"],
+		"date": ["Date:", "1994-04-30"],
+		"email": ["Email:", "lightking@fullsail.edu"],
+		"age": ["Age:", "18"],
+		"addr": ["Address:", "22993 SeaSpray Pl"],
+		"Artist": ["Artist:", "Linkin Park"],
+		"Notes": ["Notes:", "Visual Frameworks"]
+	},
+	"Birthday2": {
+		"fname": ["First Name:", "James"],
+		"lname": ["Last Name:", "Bond"],
+		"date": ["Date:", "1920-11-11"],
+		"email": ["Email:", "James_Bond@007.com"],
+		"age": ["Age:", "93"],
+		"addr": ["Address:", "0070 Spy Road"],
+		"Artist": ["Artist:", "Decyfer Down"],
+		"Notes": ["Notes:", "Secret Spy"]
+	},
+	"Birthday3": {
+		"fname": ["First Name:", "Johnny"],
+		"lname": ["Last Name:", "Smith"],
+		"date": ["Date:", "2000-04-12"],
+		"email": ["Email:", "JSmith@yahoo.com"],
+		"age": ["Age:", "13"],
+		"addr": ["Address:", "34324 Hills Parkway"],
+		"Artist": ["Artist:", "2chainz"],
+		"Notes": ["Notes:", "Little Brothers friend"]
+	}
+}
